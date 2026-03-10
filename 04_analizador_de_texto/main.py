634077b4138589fc2a33bd6c2d74239d896eb568
Angel Morales <maldonadoangel7069@gmail.com>
2026-03-10 09:51:13 -0600

    Ejercicio 2 de index: Encuentra y muestra en pantalla el índice de la primera aparición de la palabra practica

--- 04_analizador_de_texto/main.py
+++ 04_analizador_de_texto/main.py
@@ -11,3 +11,9 @@ print(resultado)
 #Encuentra y muestra en pantalla qué caracter ocupa la quinta posición dentro de la siguiente palabra: "ordenador"
 palabra = "ordenador"
 print(palabra[4])
+#Practica 2
+#Encuentra y muestra en pantalla el índice de la primera aparición de la palabra "práctica" en la siguiente frase:
+#"En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+
+print(frase.index("práctica"))
