36c49fbdfd9950c0e04303ee78e6a58b2483bad9
Angel Morales <maldonadoangel7069@gmail.com>
2026-03-22 00:33:01 -0600

    metodo index y reverse en las listas

--- 05_estructura_de_datos/main.py
+++ 05_estructura_de_datos/main.py
@@ -11,3 +11,9 @@ print(frutas.count('Kiwi')) #muestra el valor de 1
 # sirve para un propósito muy específico y sencillo: 
 # contar cuántas veces aparece un elemento determinado dentro de una lista.
 print(frutas.index('Kiwi'))
+#Como buscar la proxima fruta a partir de la posicion 4 osea el kiwi
+print(frutas.index('banana', 4)) #Nos devuelve la posicion 6
+#invertir la lista, lo que hace es modificar la lista original
+#si intentamos imprimir frutas.reverse() nos regresara none
+frutas.reverse()
+print(frutas)
