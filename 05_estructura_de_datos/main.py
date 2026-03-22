0753eec8bb2aa81dab11c0f95f1e50d960e978f0
Angel Morales <maldonadoangel7069@gmail.com>
2026-03-21 23:43:27 -0600

    Metodo count e index en las listas

--- 05_estructura_de_datos/main.py
+++ 05_estructura_de_datos/main.py
@@ -2,5 +2,12 @@
 #Listas
 frutas = ['Naranja', 'Manzana', 'Pera', 'banana', 'Kiwi', 'Manzana', 'banana']
 print(frutas)
-
-print(frutas.count('Manzana'))
+#Según la documentación oficial de Python, el método .count() 
+# sirve para un propósito muy específico y sencillo: 
+# contar cuántas veces aparece un elemento determinado dentro de 
+# una lista.
+print(frutas.count('Kiwi')) #muestra el valor de 1
+#Según la documentación oficial de Python, el método .count() 
+# sirve para un propósito muy específico y sencillo: 
+# contar cuántas veces aparece un elemento determinado dentro de una lista.
+print(frutas.index('Kiwi'))
