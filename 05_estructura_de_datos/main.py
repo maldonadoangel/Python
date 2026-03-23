a4e95c0aa9caa9bcabe5e36eec84957d7cd65a35
Angel Morales <maldonadoangel7069@gmail.com>
2026-03-22 22:32:42 -0600

    Transponer una matriz con listas de compresion y la manera clasica

--- 05_estructura_de_datos/main.py
+++ 05_estructura_de_datos/main.py
@@ -95,3 +95,20 @@ print(lista_absoluta)
 fruta_fresca = ['  banana', '  loganberry ', 'passion fruit  ']
 fruta_fresca_limpia = [fruta.strip() for fruta in fruta_fresca]
 print(fruta_fresca_limpia)
+#Listas por comprension anidadas
+matrix = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+]
+#Transponer la lista de las filas a columnas
+matrix_transpuesta = [[row[i] for row in matrix] for i in range(4)]
+print(matrix_transpuesta)
+#Lo mismo pero sin lista de compresion
+transposed = []
+for i in range(4):
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+print(transposed)
